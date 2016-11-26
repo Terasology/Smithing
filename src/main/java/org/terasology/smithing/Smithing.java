863ef78820220674aa3a56b039cd95273875be09
XTariq <xrtariq@gmail.com>
2016-11-25 18:33:35 -0700

    Initial commit for this module.
    Migrated elements from CopperAndBronze module, and applied some fixes and modifications.

---
+++ src/main/java/org/terasology/smithing/Smithing.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2016 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.smithing;
+
+public class Smithing {
+    public static final String BASIC_SMITHING_PROCESS = "Smithing:BasicSmithingProcess";
+    public static final String NOVICE_SMITHING_PROCESS = "Smithing:NoviceSmithingProcess";
+    public static final String STANDARD_SMITHING_PROCESS = "Smithing:StandardSmithingProcess";
+}
