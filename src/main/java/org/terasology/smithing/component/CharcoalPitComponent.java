4304b280fb6218da7c3a38dcaa6b34c35dbdb30e
Michael Pollind <mpollind@gmail.com>
2021-07-11 19:40:33 -0700

    chore: update components

--- src/main/java/org/terasology/smithing/component/CharcoalPitComponent.java
+++ src/main/java/org/terasology/smithing/component/CharcoalPitComponent.java
@@ -7,27 +7,50 @@ import org.terasology.gestalt.entitysystem.component.Component;
 
 public class CharcoalPitComponent implements Component<CharcoalPitComponent> {
 
-    /** The time in milliseconds in-game when the charcoal pit begins to burn, otherwise holds the last time the charcoal pit began to burn */
+    /**
+     * The time in milliseconds in-game when the charcoal pit begins to burn, otherwise holds the last time the charcoal pit began to burn
+     */
     @Replicate
     public long burnStartWorldTime;
 
-    /** The time in milliseconds in-game when the charcoal pit stops burning, otherwise holds the last time the charcoal pit finished burning */
+    /**
+     * The time in milliseconds in-game when the charcoal pit stops burning, otherwise holds the last time the charcoal pit finished
+     * burning
+     */
     @Replicate
     public long burnFinishWorldTime;
 
-    /** Minimum number of logs */
+    /**
+     * Minimum number of logs
+     */
     @Replicate
     public int minimumLogCount;
 
-    /** Maximum number of logs */
+    /**
+     * Maximum number of logs
+     */
     @Replicate
     public int maximumLogCount;
 
-    /** Number of input slots for fuel */
+    /**
+     * Number of input slots for fuel
+     */
     @Replicate
     public int inputSlotCount;
 
-    /** Number of output slots for charcoal */
+    /**
+     * Number of output slots for charcoal
+     */
     @Replicate
     public int outputSlotCount;
+
+    @Override
+    public void copy(CharcoalPitComponent other) {
+        this.burnStartWorldTime = other.burnStartWorldTime;
+        this.burnFinishWorldTime = other.burnFinishWorldTime;
+        this.minimumLogCount = other.minimumLogCount;
+        this.maximumLogCount = other.maximumLogCount;
+        this.inputSlotCount = other.inputSlotCount;
+        this.outputSlotCount = other.outputSlotCount;
+    }
 }
